769a09a6445c390b129d46124a8827bcdbe1c8ea
mnassr1658 <mnassr@aucegypt.edu>
2023-04-15 06:14:00 +0200

    adding website

---
+++ website_databse.py
@@ -0,0 +1,31 @@
+import mysql.connector
+import streamlit as st
+
+def connect_to_database():
+    
+    mydb = mysql.connector.connect(
+    host = "db4free.net",
+    user = "monasr",
+    password = "monasr1658",
+    database = "olxxdatabase",
+    )
+    
+    return mydb
+
+# print(mydb)
+# mycursor = mydb.cursor()
+# sql = """
+# select * from user;
+# """
+# mycursor.execute(sql)
+
+# result = mycursor.fetchall()
+# for r in result:
+#     print(r)
+
+mydb = connect_to_database()
+
+st.write ('''
+# Welcome to OLX Database
+''')
+
